19ceebccdd12f975eae6ded26c66f6f03a83ea79
SANTIAGOLOPEZ <santiloez6@gmail.com>
2024-08-06 05:55:00 -0300

    GOGETA-BOT
    GOGETA-BOT

--- handler.js
+++ handler.js
@@ -1630,7 +1630,7 @@ export async function participantsUpdate({ id, participants, action }) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m.conn.groupMetadata(id) || (conn.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = '';
+          let pp = 'https://github.com/santiyt65/GOGETA-BOT-MD/blob/main/src/avatar_contact.png';
           try {
             pp = await m.conn.profilePictureUrl(user, 'image');
           } catch (e) {
