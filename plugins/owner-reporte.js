8575d6a000f66405c74f05637301a93ae0cbbf3b
SANTIAGOLOPEZ <santiloez6@gmail.com>
2024-08-11 02:01:04 -0300

    GOGETA-BOT
    GOGETA-BOT

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -10,8 +10,8 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
     if (text.length < 10) throw tradutor.texto2;
     if (text.length > 1000) throw tradutor.texto3;
     const teks = `${tradutor.texto4[0]} wa.me/${m.sender.split`@`[0]}\n${tradutor.texto4[1]} ${text}\n*┴*`;
-    conn.reply('54 9 260 420-0949@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-    conn.reply('54 9 260 420-0949@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+    conn.reply('54 9 260 403-5392@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+    conn.reply('54 9 260 403-5392@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
     m.reply(tradutor.texto5);
   };
   handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
